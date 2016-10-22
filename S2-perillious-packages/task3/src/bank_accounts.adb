92b85411871df4308088005a0621ca3bfcc01757
MattiWe <matti.wiegmann@uni-weimar.de>
2016-10-22 15:11:51 +0200

    implement bank_accounts

--- S2-perillious-packages/task3/src/bank_accounts.adb
+++ S2-perillious-packages/task3/src/bank_accounts.adb
@@ -1,17 +1,29 @@
-package Bank_Accounts is
-    subtype Cents_Type is Integer;
-    type Account_Type is record
-        Balance: Cents_Type := 0;
-    end record;
-    
-    function Get_Balance(Account: Account_Type) return Cents_Type;
+package body Bank_Accounts is
+   
     -- Returns the current Balance from Account.
-    procedure Deposit(Account: in out Account_Type; Amount: Cents_Type);
-    -- Deposits Amount at the given Account.
-    procedure Withdraw(Account: in out Account_Type; Amount: Cents_Type);
-    -- Withdraws Amount from the given Account.
-    procedure Transfer(From: in out Account_Type; 
-                       To: in out Account_Type; 
-                       Amount: in Cents_Type);
-    -- Transfers Amount from Account From to Account To.
+   function Get_Balance(Account: Account_Type) return Cents_Type is
+   begin
+      return Account.Balance;
+   end Get_Balance;
+   
+    -- Deposits Amount at the given Account.   
+   procedure Deposit(Account: in out Account_Type; Amount: Cents_Type) is
+   begin
+      Account.Balance := Account.Balance + Amount;
+   end Deposit;
+  
+   -- Withdraws Amount from the given Account.
+   procedure Withdraw(Account: in out Account_Type; Amount: Cents_Type) is
+   begin
+      Account.Balance := Account.Balance - Amount;
+   end Withdraw;
+ 
+   -- Transfers Amount from Account From to Account To.
+   procedure Transfer(From: in out Account_Type; 
+                      To: in out Account_Type; 
+                      Amount: in Cents_Type) is
+   begin
+      From.Balance := From.Balance - Amount;
+      TO.Balance := To.Balance + Amount;
+   end Transfer;
 end Bank_Accounts;
--- S2-perillious-packages/task3/src/main.adb
+++ S2-perillious-packages/task3/src/main.adb
@@ -1,11 +1,11 @@
-with Deck;
 with Ada.Text_IO;
+with Bank_Accounts;
 
 procedure main is
-	begin
-	for Integer in 1..3 loop
-		Deck.Draw_New_Card;
-		Deck.Print_Latest_Card;
-		Ada.Text_IO.Put_Line(" ");
-	end loop;
+   Mattis_Account : Bank_Accounts.Account_Type;
+   Balance : Bank_Accounts.Cents_Type := 0;
+begin
+   Bank_Accounts.Deposit(Account => Mattis_Account, Amount => 54);
+   Balance := Bank_Accounts.Get_Balance(Mattis_Account);
+   Ada.Text_IO.Put_Line(Integer'Image(Balance));
 end main;
