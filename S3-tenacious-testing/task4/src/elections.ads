5f9ee414501818dc8ece7a75eea3a7d814bee60c
Nana <nana.d@hotmail.de>
2016-11-20 17:02:43 +0100

    added pre and post conditions

--- S3-tenacious-testing/task4/src/elections.ads
+++ S3-tenacious-testing/task4/src/elections.ads
@@ -7,12 +7,15 @@ package Elections is
     Num_Votes_Made: Natural := 0;
     Num_Total_Voters: Natural := 0;
 
-    procedure Initialize(Num_Voters: Natural);
+    procedure Initialize(Num_Voters: Natural) with
+    	Pre=>(Num_Voters>0),
+    	Post=>(Num_Total_Voters = Num_Voters);
     -- Resets the number of made votes and votes for all parties to 0, and 
     -- sets the number of total Voters to the given.
     procedure Vote_For(Vote: Party);
     function All_Voters_Voted return Boolean;
-    function Find_Winner return Party;
+    function Find_Winner return Party with
+    	Pre=>(All_Voters_Voted = TRUE);
     -- Returns Party with most votes assigned. 
     -- Returns None if multiple parties share the highest votes.
 end Elections;
