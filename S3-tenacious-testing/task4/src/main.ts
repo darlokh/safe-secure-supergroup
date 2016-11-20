6933176570f7c9217924751267a0a1cbfab5967a
Nana <nana.d@hotmail.de>
2016-11-20 18:55:05 +0100

    first test case

--- S3-tenacious-testing/task4/src/main.ts
+++ S3-tenacious-testing/task4/src/main.ts
@@ -1,10 +1,12 @@
 context with Ada.Text_IO;
+	with Elections; use Elections;
 
 code Ada.Text_IO.Put_Line("Begin Testing: ");
 
-***** Test (1)
-define declare
-test begin .. end
-pass boolean
+***** Test (1) Initialize
+Define	Num_Voters : Natural := 1;
+Test 	Initialize(Num_Voters);
+Pass	Num_Voters > 0
+
 
 code Ada.Text_IO.Put_Line("done");
