45bbfb370701aa3408f692f805f509b9003798a4
Nana <nana.d@hotmail.de>
2016-12-05 10:19:31 +0100

    Bonus task g.ads

--- S4-horrifying-hoare/task3/g.ads
+++ S4-horrifying-hoare/task3/g.ads
@@ -4,7 +4,20 @@ function G(N: Natural; K: Natural) return Natural is
     Z: Natural; 
 begin
     X := F(K);
+-- [X = F(K)] Rule: Assignment
     Y := F(N-K);
+-- [Y = F(N-K)] Rule: Assignment
     Z := F(N);
+-- [Z = F(N)] Rule: Assignment
     return Z / (X * Y);
 end G;
+
+-- Pre => (N <= Natural'Last)
+      and (K <= Natural'Last)
+      and (X <= Natural'Last)
+      and (Y <= Natural'Last)
+      and (Z <= Natural'Last)
+-- Post => (X = (K-1)!)
+       and (Y = (N-K-1)!)
+       and (Z = (N-1)!)
+       and (Z/(X*Y)=(N-1)! / (K-1)! * (N-K-1)!)
