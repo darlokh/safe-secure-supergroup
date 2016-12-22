9950ac59febdcd578eeaab3eb88cdd4840a5abd6
MattiWe <matti.wiegmann@uni-weimar.de>
2016-12-22 23:01:55 +0100

    merge sort implementation

--- S6-terrific-tasks/task4/src/main.adb
+++ S6-terrific-tasks/task4/src/main.adb
@@ -10,17 +10,16 @@ procedure Main is
    Test_Out_Array : constant Integer_Sort.Array_Access_Type
      := new Integer_Sort.Array_Type(1..9);
 begin
-   Test_In_Array.all := (1,2,3,4, others => 0);
-   Test_Out_Array.all := (others => 0);
+   Test_In_Array.all := (8,1,6,6,4,3,2,7,9);
 
    Integer_Sort.Parallel_Merge_Sort(Test_In_Array, Test_Out_Array);
 
    declare
-         test_int : Integer := 0;
-      begin
-         for i in Test_Out_Array'First .. Test_Out_Array'Last loop
-            test_int := Test_Out_Array.all(i);
-            Ada.Text_IO.Put_Line(test_int'Image);
-         end loop;
-      end;
+      test_int : Integer := 0;
+   begin
+      for i in Test_Out_Array'First .. Test_Out_Array'Last loop
+         test_int := Test_Out_Array.all(i);
+         Ada.Text_IO.Put_Line(test_int'Image);
+      end loop;
+   end;
 end Main;
--- S6-terrific-tasks/task4/src/parallel_merge_sort.adb
+++ S6-terrific-tasks/task4/src/parallel_merge_sort.adb
@@ -1,15 +1,54 @@
-with Ada.Text_IO;
-
 package body Parallel_Merge_Sort is
 
    procedure Parallel_Merge_Sort(Input: Array_Access_Type;
                                  Result: Array_Access_Type) is
+      left_in, left_out 
+      : constant Array_Access_Type := new Array_Type(1..Input'Length/2);
+      right_in, right_out 
+      : constant Array_Access_Type := new Array_Type(1..Input'Length/2+(Input'Length mod 2));
    begin
+      -- only 1 left
+      if Input'Length = 1 then
+         Result(1) := Input(1);
+         return;
+      end if;
+      
+      -- split list
+      left_in.all := Input(Input'First..Input'Length/2);
+      right_in.all := Input((Input'Length/2)+1..Input'Last);
+      
+      -- recursive call
+      Parallel_Merge_Sort(left_in, left_out);
+      Parallel_Merge_Sort(right_in, right_out);
+      
+      -- merge
       declare 
-         test_int : Integer := 0;
+         left_attended : Natural := 1;
+         right_attended : Natural := 1;
       begin
-         for i in Input'First .. Input'Last loop
-            Result(i) := Input(i);
+         for i in Result'First .. Result'Last loop
+            if left_attended <= left_out'Length and 
+              right_attended <= right_out'Length then
+               if left_out(left_attended) < right_out(right_attended) or
+                 left_out(left_attended) = right_out(right_attended) then
+                  Result(i) := left_out(left_attended);
+                  left_attended := left_attended + 1;
+               else
+                  Result(i) := right_out(right_attended);
+                  right_attended := right_attended + 1;
+               end if;
+            elsif left_attended <= left_out'Length and 
+              right_attended > right_out'Length then
+               Result(i) := left_out(left_attended);
+               left_attended := left_attended + 1;
+            elsif left_attended > left_out'Length and 
+              right_attended <= right_out'Length then
+               Result(i) := right_out(right_attended);
+               right_attended := right_attended + 1;
+            else
+               -- should throw "this_should_never_happen_exception"
+               return;
+            end if;
          end loop;
       end;
    end Parallel_Merge_Sort;
