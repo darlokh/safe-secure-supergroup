4418c77f82e748c815bae9d09a4b3f8bf906f6f7
MattiWe <matti.wiegmann@uni-weimar.de>
2016-12-29 23:37:02 +0100

    cmd prep

--- S6-terrific-tasks/task4/src/main.adb
+++ S6-terrific-tasks/task4/src/main.adb
@@ -1,7 +1,10 @@
 with Ada.Text_IO;
 with Parallel_Merge_Sort;
+with GNAT.Strings; use GNAT.Strings;
+with GNAT.Command_Line; use GNAT.Command_Line;
 
 procedure Main is
+   -- Declare lists to sort and instanciate sort package.
    package Integer_Sort is new Parallel_Merge_Sort(Item_Type => Integer,
                                                    "=" => "=",
                                                    "<" => "<");
@@ -9,10 +12,34 @@ procedure Main is
      := new Integer_Sort.Array_Type(1..9);
    Test_Out_Array : constant Integer_Sort.Array_Access_Type
      := new Integer_Sort.Array_Type(1..9);
-begin
-   Test_In_Array.all := (8,1,6,6,4,3,2,7,9);
 
-   Integer_Sort.Parallel_Merge_Sort(Test_In_Array, Test_Out_Array);
+   -- Declare command line switches and arguments
+   Config : Command_Line_Configuration;
+   In_List_Path : aliased String_Access;
+   Out_List_Path : aliased String_Access;
+   Time_Parameter : aliased Integer := 0;
+
+begin
+   Define_Switch (Config, In_List_Path'Access, "-i:",
+                  Help => "-i missing, need path to input file");
+   Define_Switch (Config, Out_List_Path'Access, "-o:",
+                  Help => "-o missing, need path to output file");
+   Define_Switch (Config, Time_Parameter'Access, "-t:",
+                  Help => "-t missing, set timeout as integer");
+
+   Getopt(Config);
+
+   Ada.Text_IO.Put_Line(Item => In_List_Path.all);
+   Ada.Text_IO.Put_Line(Item => Out_List_Path.all);
+   Ada.Text_IO.Put_Line(Item => Time_Parameter'Image);
+
+   Test_In_Array.all := (8,1,6,6,4,3,2,7,9);
+   declare
+      test_task : Integer_Sort.Sort_Task;
+   begin
+      test_task.Set_Unsorted(Test_In_Array);
+      test_task.Set_Sorted(Test_Out_Array);
+   end;
 
    declare
       test_int : Integer := 0;
