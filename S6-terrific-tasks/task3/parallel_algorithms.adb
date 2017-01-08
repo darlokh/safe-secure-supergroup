6a991ef493e0591b6c5d369eb4eb0f8a314b8c28
Nana <nana.d@hotmail.de>
2017-01-08 15:21:33 +0100

    tasks working now but problems with generic type conversion

---
+++ S6-terrific-tasks/task3/parallel_algorithms.adb
@@ -0,0 +1,60 @@
+package body Parallel_Algorithms is
+
+   task body Adder is
+      My_Array        : Array_Access_Type;
+      Id_Low, Id_High : Natural;
+   begin
+
+      accept Set_Slice(Start_Index, End_Index : in Natural; Item : in not null Array_Access_Type; Sum : out Item_Type) do
+         Id_Low := Start_Index;
+         Id_High := End_Index;
+         My_Array := Item;
+
+         for I in Id_Low..Id_High loop
+            Sum := Sum + My_Array(I);
+         end loop;
+
+         Result.Accumulate(Item_Type(Sum));
+
+      end Set_Slice;
+      
+   end Adder;
+
+   --helper
+   protected body Result is
+
+      procedure Accumulate (Item : in Item_Type) is
+      begin
+         Sum := Sum + Item;
+      end Accumulate;
+
+      function Report return Item_Type is
+      begin
+         return Item_Type(Sum);
+      end Report;
+
+   end Result;
+
+
+   --Parallel_Sum
+   procedure Parallel_Sum(Input: Array_Access_Type; Result : out Item_Type ) is
+      Task_Left, Task_Right : Adder;
+      temp : Natural := 0;
+      value : Natural := 0;
+   begin
+      if Input'Last mod 2 = 0 then
+         temp := Input'Last / 2;
+         Task_Left.Set_Slice(Start_Index => 0, End_Index => temp, Item => Input, Sum => Item_Type(value));
+         temp := (Input'Last / 2) + 1;
+         Task_Right.Set_Slice(Start_Index => temp, End_Index => Input'Last, Item => Input, Sum => Item_Type(value));
+      else
+         temp := (Input'Last -1) / 2;
+         Task_Left.Set_Slice(Start_Index => 0, End_Index => temp, Item => Input, Sum => Item_Type(value));
+         temp := (Input'Last +1) / 2;
+         Task_Right.Set_Slice(Start_Index => temp, End_Index => Input'Last, Item => Input, Sum => Item_Type(value));
+      end if;
+   end Parallel_Sum;
+
+   
+
+end Parallel_Algorithms;
--- S6-terrific-tasks/task3/parallel_algorithms.ads
+++ S6-terrific-tasks/task3/parallel_algorithms.ads
@@ -1,11 +1,21 @@
 generic
-    type Item_Type is private;
-    with function "+"(Left: Item_Type; Right: Item_Type) return Item_Type;
+   type Item_Type is private;
+   with  function "+" (Left: Item_Type ; Right : Item_Type ) return Item_Type ;
 package Parallel_Algorithms is
-    
-    type Array_Type is array(Natural range <>) of Item_Type;
-    type Array_Access_Type is access all Array_Type;
 
-    procedure Parallel_Sum(Input: Array_Access_Type; Result: out Item_Type);
-    
+   type Array_Type is array(Natural range <>) of Item_Type;
+   type Array_Access_Type is access all Array_Type;
+
+   task type Adder is
+      Entry Set_Slice(Start_Index, End_Index : in Natural; Item : in not null Array_Access_Type; Sum : out Item_Type);
+   end Adder;
+
+   procedure Parallel_Sum(Input: Array_Access_Type; Result : out Item_Type );
+
+   protected Result is
+      procedure Accumulate(Item : in Item_Type);
+   private
+      Sum : Item_Type;
+   end Result;
+
 end Parallel_Algorithms;
