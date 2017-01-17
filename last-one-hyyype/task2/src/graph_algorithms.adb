f54716ea5107b16dadb73bec211f51a02da2f57c
MattiWe <matti.wiegmann@uni-weimar.de>
2017-01-18 00:02:35 +0100

    kruskal w/o prior sort

--- last-one-hyyype/task2/src/graph_algorithms.adb
+++ last-one-hyyype/task2/src/graph_algorithms.adb
@@ -10,13 +10,22 @@ package body Graph_Algorithms is
 
    -- Kruskal
    function Find_Min_Spanning_Tree(G: Graph_Type) return Graph_Type is
-
-      G_In : Graph_Type;
+      G_Out : Graph_Type;
    begin
-      -- Note: edge-vector schould be sorted asc.
+      -- Note: edge-vector schould be sorted asc. but thats to hard for me
       -- take first edge, add to span if vertices are not in connected subgraph
 
-      G_In := G;
-      return G;
+      -- check if connected
+      for E of G.Edges loop
+         if(not (G_Out.Vertices.Contains(E.From)
+                 and G_Out.Vertices.Contains(E.To))) then
+            -- TODO check exceptions
+            G_Out.Add_Vertex(E.From);
+            G_Out.Add_Vertex(E.To);
+            G_Out.Add_Edge(E.From, E.To, E.Weight);
+         end if;
+      end loop;
+
+      return G_out;
    end Find_Min_Spanning_Tree;
 end Graph_Algorithms;
